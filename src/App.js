c60c13bead9e31e63483ac780ca47b9fb26a0b1b
Adithya <aadithyan882001@gmail.com>
2024-02-23 23:46:33 +0530

    added functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
+import { Button,Stack,TextField} from '@mui/material';
 
 function App() {
+  const [background,setChangeBackground] = useState('')
+  const [customColor,setCustomColor] = useState('')
+
+  const changeColor = (color) => {
+    setChangeBackground(color)
+  }
+
+  const handleChange= (value) => {
+    setCustomColor(value)
+  }
+
+  const customChange = () => {
+    setChangeBackground(customColor)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="display" style={{background}}>
+
+      <Stack direction={'row'} spacing={2}>
+      <Button onClick = {()=>changeColor('green')} variant="contained" color="success">
+        Green
+      </Button>
+      <Button onClick = {()=>changeColor('blue')} variant="contained" color="primary">
+        Blue
+      </Button>
+      <Button onClick = {()=>changeColor('red')} variant="contained" color="error">
+        Red
+      </Button>
+      <Button onClick = {()=>changeColor('violet')} variant="contained" color="secondary">
+        Violet
+      </Button>
+      </Stack>
+
+      <div className='inpt-div'>
+        <TextField onChange={(e)=>handleChange(e.target.value)} id="standard-basic" label="Custom Color" variant="standard" />
+        <Button onClick={customChange} variant="outlined" color="error">
+          Change
+        </Button>
+      </div>
     </div>
   );
 }
